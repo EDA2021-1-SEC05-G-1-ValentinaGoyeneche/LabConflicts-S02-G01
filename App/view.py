efa379d8a850ee2ab6129f81a76e7f297ce3270f
cristianfonseca03 <cristian.lfs@gmail.com>
2021-02-07 11:25:58 -0500

    cambios del Estudiante 2

--- App/view.py
+++ App/view.py
@@ -81,7 +81,10 @@ while True:
 
 
         # TO-DO: Modificación de Est-2 en el Lab 2
-
+    elif int(inputs[0]) == 3:
+        print("Cargando información de Book-Tags...")
+        booktags = loadBooksTags()
+        print('Total de Book-Tags cargados: ' + str(lt.size(booktags)))
     elif int(inputs[0]) == 2:
         print("Cargando información de tags....")
         tags = loadTags()
